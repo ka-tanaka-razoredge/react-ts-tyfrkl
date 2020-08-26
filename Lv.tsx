803b9d8095e7233abbff707e5663018f268df4c5
ka-tanaka-razoredge <software@razor-edge.net>
2020-08-27 07:19:34 +0900

    1 file modified

--- Lv.tsx
+++ Lv.tsx
@@ -9,10 +9,7 @@ export const Lv = React.forwardRef((props: LvProps, ref) => {
 
   // [変数, 変数に値をセットする関数]
   const [text, setText] = useState('');
-
-  addEventListener('An item arrives', (e) => {
-  });
-
+  
   useEffect(() => {
     ref.current.addEventListener('An item arrives', (e) => {
       console.log(e);
